884a26a8332e97c824e69426c67596bf88f08fcf
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-12 22:55:38 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 exports.Prefix = `?`;//your prefix for bot
-exports.Token = `ODQxMzMxNzIyMDE5OTMwMTcz.YJlNOQ.KBfkJSUNiOTA37h1aU5W5DzGJlM`;//your token 
+exports.Token = `ODE3MTE1NTA5OTAwMTgxNTc2.YEE0Hg.GrJTD85GhCVmGih8Z5itRBmycnQ`;//your token 
 exports.Color = `RED`;//color   of embed
