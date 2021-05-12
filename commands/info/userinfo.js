9b3a170cb3db749c71ac6084bab5dc98d1e57524
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-12 22:46:56 +0300

    Add files via upload

---
+++ commands/info/userinfo.js
@@ -0,0 +1,45 @@
+ const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+const { Color } = require("../../config.js");
+
+module.exports = {
+  name: "userinfo",
+  aliases: ["memberinfo", "whois"],
+  description: "Informasi User!",
+  usage: "Userinfo | <Mention User>",
+  run: async (client, message, args) => {
+    //Start
+    message.delete();
+    let member = message.mentions.users.first() || message.member;
+
+    const statuses = {
+      online: "Online",
+      dnd: "Do Not Disturb",
+      idle: "Idle",
+      offline: "Offline/Invisible"
+    };
+
+    const embed = new MessageEmbed()
+      .setTitle(member.user.username + " Information!")
+      .setColor(Color)
+      .setThumbnail(member.user.displayAvatarURL())
+      .addField("Nama", member.user.tag, true)
+      .addField("ID", `${member.id}`, true)
+      .addField("Status", statuses[member.presence.status], true)
+      .addField(
+        `Roles Count`,
+        message.guild.members.cache.get(member.user.id).roles.cache.size ||
+          "Tidak ada Role!",
+        true
+      )
+      .addField(`Avatar Url`, `[Link](${member.user.displayAvatarURL()})`, true)
+      .addField("Masuk Server pd", member.joinedAt.toDateString())
+      .addField("Main Discord pd", member.user.createdAt.toDateString())
+      .setFooter(`Requested by ${message.author.username}`)
+      .setTimestamp();
+
+    message.channel.send(embed);
+
+    //End
+  }
+};
---
+++ commands/info/weather.js
@@ -0,0 +1,46 @@
+const Discord = require("discord.js");
+const weather = require("weather-js");
+const { MessageEmbed } = require("discord.js");
+const { Color } = require("../../config.js");
+
+module.exports = {
+  name: "weather",
+  aliases: [],
+  description: "Menampilkan cuaca di lokasi mu!",
+  usage: "Weather <Location>",
+  run: async (client, message, args) => {
+    //Start
+    message.delete();
+
+    if (!args[0]) return message.channel.send("Please Masukkan Nama Lokasi nya!");
+
+    weather.find({ search: args.join(" ") }, function(error, result) {
+      if (error) return message.channel.send(`Ada Sesuatu Yang Salah, Coba Lagi!`);
+
+      if (result === undefined || result.length === 0)
+        return message.channel.send(
+          `Lokasi Tidak Valid, Berikan Lokasi yang Valid!`
+        );
+
+      var current = result[0].current;
+      var location = result[0].location;
+
+      const Weather = new Discord.MessageEmbed()
+        .setColor(Color)
+        .setTitle(`${location.name} Weather!`)
+        .setDescription(`${current.skytext}`)
+        .setThumbnail(current.imageUrl)
+        .addField("Degree Type", location.degreetype, true)
+        .addField("Temperature", `${current.temperature}°`, true)
+        .addField("Humidity", `${current.humidity}%`, true)
+        .addField("Wind", current.winddisplay, true)
+        .addField("Feels Like", `${current.feelslike}°`, true)
+        .addField("Timezone", `UTC${location.timezone}`, true)
+        .setTimestamp();
+
+      message.channel.send(Weather);
+    });
+
+    //End
+  }
+};
