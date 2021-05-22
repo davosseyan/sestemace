0801aef6ef25501640f3b600f7225a1d8c5ebdac
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-22 17:32:57 +0300

    Create undeafen.js

---
+++ commands/moderation/undeafen.js
@@ -0,0 +1,16 @@
+var deafuser = msg.mentions.members.first();
+    var muteReason = msg.content.slice(prefix.length + 27);
+    if (!msg.member.hasPermission("MANAGE_MESSAGES")) return msg.channel.send("pewist ba role a bo anjamdane amkara");
+    if (!deafuser) return msg.channel.send("kasek mention bka");
+ 
+  if (!msg.guild.member(client.user.id).hasPermission("MANAGE_ROLES")) return msg.channel.send("sarata rolem bare inja bakarm hena");
+    var muteEmbed = new Discord.MessageEmbed() 
+    .setTitle("Undeafen")
+    .addField("Undeafen user", deafuser)
+    .setFooter(`Undeafen by ${msg.author.tag}`)
+    .setTimestamp()
+      deafuser.voice.setDeaf(false)
+    msg.channel.send(muteEmbed);
+ 
+  }
+}
