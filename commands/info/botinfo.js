ce8c6729ccd29a09ae1a0fcf5c716c57512efc3a
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-22 18:03:48 +0300

    Update botinfo.js

--- commands/info/botinfo.js
+++ commands/info/botinfo.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-  name: "ping",
+  name: "botinfo",
   aliases: [],
   description: "Pong!",
   usage: "Ping",
