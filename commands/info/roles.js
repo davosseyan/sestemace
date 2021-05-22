7776bc25303a64f98dea0794f5f25f1872b7b607
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-22 14:26:03 +0300

    Update roles.js

--- commands/info/roles.js
+++ commands/info/roles.js
@@ -1,3 +1,12 @@
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "roles",
+  aliases: [],
+ 
+  run: async(client, message, args) => {
+
 let roles = message.guild.roles.cache.map(r => `> ${r.name} `).join("\n"); 
  let embed = new Discord.MessageEmbed()
   .setTitle("Server Roles") 
