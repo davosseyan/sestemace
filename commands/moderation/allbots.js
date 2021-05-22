3435ad15aa8f304949a2d6e992dd51f0041a00d2
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-22 13:44:27 +0300

    Create allbots.js

---
+++ commands/moderation/allbots.js
@@ -0,0 +1,23 @@
+let i = 1;
+    
+    const botssize = message.guild.members.cache
+      .filter(m => m.user.bot)
+      .map(m => `${i++} - <@${m.id}>`);
+    
+    
+    const embed = new Discord.MessageEmbed()
+      .setAuthor(message.author.tag, message.author.avatarURL())
+      .setColor("BLACK")
+      .setDescription(
+        `**${
+          message.guild.members.cache.filter(m => m.user.bot).size
+        } You were found on this server**
+${botssize.join("\n")}`
+      )
+      .setFooter(client.user.username, client.user.avatarURL())
+      .setTimestamp();
+    
+    
+    message.channel.send(embed);
+  }
+}
