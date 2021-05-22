ae97d8d119585778bea9c563d94795fe2c6eebe6
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-22 17:46:28 +0300

    Update invite.js

--- commands/info/invite.js
+++ commands/info/invite.js
@@ -1,8 +1,17 @@
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "invite",
+  aliases: ["link"],
+  description: "Help Command!",
+  usage: "o/Help | <Command Name>",
+  run: async(client, message, args) => {
 let embed = new Discord.MessageEmbed()
       .setColor("BLACK")
-      .setTitle(bot.reva.get(data.lang, "general","invite_embed"))
+      .setTitle(`link Bot`)
       .setURL(
-        "https://discord.com/api/oauth2/authorize?client_id=797839831316824074&permissions=8&scope=bot"
+        ""
       )
       .setFooter(message.author.username, message.author.avatarURL());
     message.channel.send(embed);
