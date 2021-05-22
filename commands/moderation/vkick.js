4ac4d1a1de0447b1346282fda43ddab8f8113ab5
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-22 14:37:09 +0300

    Update vkick.js

--- commands/moderation/vkick.js
+++ commands/moderation/vkick.js
@@ -6,25 +6,23 @@ module.exports = {
   aliases: [],
  
   run: async (client, message, args) => {
-    let args = message.content.split(" ");
-    let user = message.guild.member(
-      message.mentions.users.first() || message.guild.members.cache.get(args[1])
-    );
-    if (!message.channel.guild || message.author.bot) return;
-    if (!message.guild.member(message.author).hasPermission("MOVE_MEMBERS"))
-      return message.channel.send("Please Check Your Permission");
-    if (!message.guild.member(client.user).hasPermission("MOVE_MEMBERS"))
-      return message.channel.send("Please Check My Permission");
-    if (!message.member.voice.channel)
-      return message.channel.send("Your are not in voice channel");
-    if (!user) return message.channel.send(`**>>> ${prefix}vkick <@mention or id>**`);
-    if (!message.guild.member(user).voice.channel)
-      return message.channel.send(
-        `**${user.user.username}** Has not in Voice channel`
-      );
-    await user.voice.kick()
-    message.channel.send(
-      `**${user.user.username}** has been kicked from **Voice Channel**`
-    )
-  }
+        if (!message.guild.me.hasPermission(["ADMINISTRATOR"]))
+      return message.channel.send(
+        "I Don't Have Proper Permissions To Use This Command!"
+      );
+
+    if (!message.mentions.members.first())
+      return message.channel.send(
+        `Please Mention User That You Want To Kick From Voice Channel!`
+      );
+
+    let { channel } = message.mentions.members.first().voice;
+
+    if (!channel)
+      return message.channel.send(`User Is Not In Any Voice Channel!`);
+
+    message.mentions.members.first().voice.kick();
+    
+    message.channel.send(`User Has Been Kicked From Voice Channel!`)
+  }
 }
