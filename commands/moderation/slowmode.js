0de97990906365b445b86f03ce00a6b73cc826be
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-22 14:07:51 +0300

    Create slowmode.js

---
+++ commands/moderation/slowmode.js
@@ -0,0 +1,22 @@
+if (!message.guild.member(message.author).hasPermission("MANAGE_CHANNELS"))
+      return message.channel.send("🧐 - Please Check Your Permission");
+    if (
+      !message.guild
+        .member(message.client.user)
+        .hasPermission("MANAGE_CHANNELS")
+    )
+      return message.channel.send(
+        "🧐 - Please Check My Permission to can edit in this channel."
+      );
+
+    let time = message.content
+      .split(" ")
+      .slice(1)
+      .join(" ");
+    if (isNaN(time)) return message.channel.send("**🧐 - Its not a time**");
+    if (!time)
+      return message.channel.send("**🧐 - Please Type a New SlowMode**");
+    message.channel.setRateLimitPerUser(time);
+    message.channel.send("**Done Changed A SlowMode To: " + time + "**");
+  }
+}
