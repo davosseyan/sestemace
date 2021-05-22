b77e8832a34779f65d891e2be0b2e27820ea051f
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-22 14:14:04 +0300

    Create vunmute.js

---
+++ commands/moderation/vunmute.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "vunmute",
+  aliases: [],
+  description: "Ban A Member!",
+  usage: "Ban <Mention Member>",
+  run: async (client, message, args) => {
+if (!message.member.hasPermission("MUTE_MEMBERS"))
+      return message.channel
+        .sendMessage("**You Need MUTE MEMBER**❌ ")
+        .then(m => m.delete(5000));
+    if (!message.guild.member(client.user).hasPermission("MUTE_MEMBERS"))
+      return message
+        .reply("**I dont`MUTE_MEMBERS` Permission**")
+        .then(msg => msg.delete(6000));
+
+    if (message.mentions.users.size === 0) {
+      return message.reply("Menition user");
+    }
+    let muteMember = message.guild.member(message.mentions.users.first());
+    if (!muteMember) {
+      return message.reply("Restart");
+    }
+    muteMember.setMute(false);
+    if (muteMember) {
+      message.channel.sendMessage("Un mute member");
+    }
+  }
+}
