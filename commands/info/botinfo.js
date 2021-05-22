08b31068d7d97bbea4c3ff2ed48e5e5e81feb5cf
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-22 17:43:34 +0300

    Create botinfo.js

---
+++ commands/info/botinfo.js
@@ -0,0 +1,15 @@
+const embed = new Discord.MessageEmbed()
+.setAuthor(client.user.username,client.user.avatarURL())
+.setThumbnail(client.user.avatarURL())
+.setColor("")
+.setTitle(` ${client.user.username} `)
+.addField('servers', `**${client.guilds.cache.size}**`, true)
+.addField('channels', `**${client.channels.cache.size}**`, true)
+.addField('Users', `**${client.users.cache.size}**`, true)
+.addField('My Name' , `**${client.user.tag}**` , true)
+.addField('My ID' , `**${client.user.id}**` , true)
+.addField('Owner Bot',`Robot.MAX`,true)
+
+msg.channel.send(embed);
+}
+}
