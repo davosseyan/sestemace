37073cf677735bb233652663042da1e58d2ed256
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-22 17:35:38 +0300

    Create roleinfo.js

---
+++ commands/moderation/roleinfo.js
@@ -0,0 +1,25 @@
+let args = message.content.split(" ").slice(1).join(" ");
+if (!args[0]) return message.channel.send("**Please Mention A Role!**")
+        let role = message.mentions.roles.first() || message.guild.roles.cache.get(args[0]) || message.guild.roles.cache.find(r => r.name.toLowerCase() === args.join(' ').toLocaleLowerCase());
+        if (!role) return message.channel.send("**Please Enter A Valid Role!**");
+        const status = {
+            false: "No",
+            true: "Yes"
+        }
+        let roleembed = new Discord.MessageEmbed()
+            .setColor("#00ff00")
+            .setAuthor("Role Info")
+            .setThumbnail(message.guild.iconURL())
+            .addField("**ID**", `\`${role.id}\``, true)
+            .addField("**Name**", role.name, true)
+            .addField("**Hex**", role.hexColor)
+            .addField("**Members**", role.members.size)
+            .addField("**Position**", role.position)
+            .addField("**Mentionable**", status[role.mentionable])
+              .addField("**Time Create**",role.createdAt.toLocaleString(),true)
+        .setFooter(message.member.displayName, message.author.displayAvatarURL())
+            .setTimestamp()
+ 
+        message.channel.send(roleembed);
+    }
+}
