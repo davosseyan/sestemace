9871eb08a193268f724cba7980d4f13d83e82c6f
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-22 17:33:56 +0300

    Update undeafen.js

--- commands/moderation/undeafen.js
+++ commands/moderation/undeafen.js
@@ -1,3 +1,12 @@
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "undeafen",
+  aliases: [],
+  description: "Mute A User!",
+  usage: "Mute <Mention User> | <Reason>",
+  run: async (client, msg, args) => {
 var deafuser = msg.mentions.members.first();
     var muteReason = msg.content.slice(prefix.length + 27);
     if (!msg.member.hasPermission("MANAGE_MESSAGES")) return msg.channel.send("pewist ba role a bo anjamdane amkara");
