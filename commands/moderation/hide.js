3e3c73b2d936d5bd25201e39e40848620332c033
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-23 19:03:20 +0300

    Update hide.js

--- commands/moderation/hide.js
+++ commands/moderation/hide.js
@@ -11,11 +11,19 @@ module.exports = {
       return message.channel.send(
         `>>> \`\`\`You Don't have the permission : \`\`\` \n\n **\`MANAGE_CHANNELS\`**`
       );
+    let channel = message.mentions.channels.first();
+    let channel_find = message.guild.channels.cache.find(
+      ch => ch.id == channel
+    );
+    if (!channel)
+      return message.channel.send(
+        `**True Use CMD: ${prefix}hide \`<MentionChannel>\`**`
+      );
+    if (!channel_find)
+      return message.channel.send(`**:x: | Error, Not Found**`);
     channel_find.updateOverwrite(message.guild.id, {
       READ_MESSAGES: false
-    })
- .then(() => {
-    message.channel.send(`**Done Has Been Hide Channel**`);
     });
+    message.channel.send(`**Done Has Been Hide Channel**`);
   }
 }
