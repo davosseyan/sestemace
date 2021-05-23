07eff333ac4aac1f969fe4ccc64d50b9dc2f3a5d
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-23 18:50:08 +0300

    Create show.js

---
+++ commands/moderation/show.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "show",
+  aliases: [],
+  description: "Ban A Member!",
+  usage: "Ban <Mention Member>",
+  run: async (client, message, args) => {
+ if (!message.member.hasPermission("MANAGE_CHANNELS"))
+      return message.channel.send(
+        `>>> \`\`\`You Don't have the permission : \`\`\` \n\n **\`MANAGE_CHANNELS\`**`
+      );
+    let channel = message.mentions.channels.first();
+    let channel_find = message.guild.channels.cache.find(
+      ch => ch.id == channel
+    );
+    if (!channel)
+      return message.channel.send(
+        `**True Use CMD: ${prefix}show \`<MentionChannel>\`**`
+      );
+    if (!channel_find)
+      return message.channel.send(`**:x: | Error, Not Found**`);
+    channel.updateOverwrite(message.guild.id, {
+      READ_MESSAGES: true
+    });
+    message.channel.send(`**Done Has Been Showed Channel**`);
+  }
+}
