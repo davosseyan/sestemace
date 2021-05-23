95dee949d8840dfcbb338487147ca5996533c885
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-23 19:16:26 +0300

    Update userinfo.js

--- commands/info/userinfo.js
+++ commands/info/userinfo.js
@@ -12,31 +12,22 @@ module.exports = {
     let member = message.mentions.users.first() || message.member;
 
     const statuses = {
-      online: "Online",
-      dnd: "Do Not Disturb",
-      idle: "Idle",
-      offline: "Offline/Invisible"
+      online: "online",
+      dnd: "do not disturb",
+      idle: "idle",
+      offline: "offline"
     };
 
     const embed = new MessageEmbed()
-      .setTitle(member.user.username + " Information!")
+      .setTitle(`member.user.displayAvatarURL()`,`${message.author.username}`)
       .setColor("")
       .setThumbnail(member.user.displayAvatarURL())
-      .addField("Nama", member.user.tag, true)
+      .addField("**Join Date**", member.joinedAt.toDateString())
+      .addField("**Create Date**", member.user.createdAt.toDateString())
+      .addField("**Other Information**",``)
       .addField("ID", `${member.id}`, true)
+      .addField("Nama", member.user.tag, true)
       .addField("Status", statuses[member.presence.status], true)
-      .addField(
-        `Roles Count`,
-        message.guild.members.cache.get(member.user.id).roles.cache.size ||
-          "Tidak ada Role!",
-        true
-      )
-      .addField(`Avatar Url`, `[Link](${member.user.displayAvatarURL()})`, true)
-      .addField("Masuk Server pd", member.joinedAt.toDateString())
-      .addField("Main Discord pd", member.user.createdAt.toDateString())
-      .setFooter(`Requested by ${message.author.username}`)
-      .setTimestamp();
-
     message.channel.send(embed);
 
     //End
