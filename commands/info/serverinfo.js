4b63359b862cebd16923b5eadb3fc2b5972e1d13
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-23 16:53:37 +0300

    Update serverinfo.js

--- commands/info/serverinfo.js
+++ commands/info/serverinfo.js
@@ -10,23 +10,41 @@ module.exports = {
     //Start
    
     const guild = message.guild;
+    const channels = message.guild.channels.cache.size;
     const Roles = guild.roles.cache.size || "No Roles!";
     const Members = guild.memberCount;
-  
+  let region;
+        switch (message.guild.region) {
+            case "europe":
+                region = 'Europe';
+                break;
+            case "us-east":
+                region = 'us-east'
+                break;
+            case "us-west":
+                region = 'us-west';
+                break;
+            case "us-south":
+                region = 'us-south'
+                break;
+            case "us-central":
+                region = 'us-central'
+                break;
+        }
+ 
 
     const embed = new MessageEmbed()
       .setTitle(guild.name + " Info")
       .setColor("")
       .setThumbnail(guild.iconURL())
-      .addField(`ID`, `${guild.id}`, true)
-      .addField(`Created At`, guild.createdAt.toDateString())
-      .addField(`Owner`, `${message.guild.owner}`, true)
-      .addField(`Members`, Members, true)
-      .addField(`Server Online`, `${message.guild.members.cache.filter(m => m.user.presence.status == "online").size}`)
-      .addField(`Roles`, Roles, true)
-     
-      .setFooter(`Requested by ${message.author.username}`)
-      .setTimestamp();
+      .addField(`🆔**__Server ID:__**`, `${guild.id}`, true)
+      .addField(`📆**__Created On:__**`, guild.createdAt.toDateString())
+      .addField(`👑**__Owned by:__**`, `${message.guild.owner}`, true)
+      .addField(`👥**__Members:__**`, Members, true)
+      .addField(`**__Server Online__**`, `${message.guild.members.cache.filter(m => m.user.presence.status == "online").size}`)
+      .addField(`💬**__Channels:__**`, `(${channels})`)
+      .addField(`**__Region__**:`, region, true)
+      .addField(`**__Roles__**`, Roles, true)
 
     message.channel.send(embed);
 
