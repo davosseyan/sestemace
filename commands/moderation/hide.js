ff32a0732f257d241338a9e8c17673f1c0c42c39
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-23 18:48:48 +0300

    Create hide.js

---
+++ commands/moderation/hide.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "hide",
+  aliases: [],
+  description: "Ban A Member!",
+  usage: "Ban <Mention Member>",
+  run: async (client, message, args) => {
+ if (!message.member.hasPermission("MANAGE_CHANNELS"))
+      return message.channel.send(
+        `>>> \`\`\`You Don't have the permission : \`\`\` \n\n **\`MANAGE_CHANNELS\`**`
+      );
+    let channel = message.mentions.channels.first();
+    let channel_find = message.guild.channels.cache.find(
+      ch => ch.id == channel
+    );
+    if (!channel)
+      return message.channel.send(
+        `**True Use CMD: ${prefix}hide \`<MentionChannel>\`**`
+      );
+    if (!channel_find)
+      return message.channel.send(`**:x: | Error, Not Found**`);
+    channel_find.updateOverwrite(message.guild.id, {
+      READ_MESSAGES: false
+    });
+    message.channel.send(`**Done Has Been Hide Channel**`);
+  }
+}
