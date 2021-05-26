23470085e176a6e30e8c1931de11810045b19a80
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-05-26 12:09:26 +0300

    Update vmute.js

--- commands/moderation/vmute.js
+++ commands/moderation/vmute.js
@@ -9,15 +9,15 @@ module.exports = {
   run: async (client, message, args) => {
 if (!message.member.hasPermission("MUTE_MEMBERS"))
       return message.channel
-        .sendMessage("**You dont Have premisssion Mutevoicr** ")
+        .sendMessage("You dont Have premisssion mute voice")
         .then(m => m.delete(5000));
     if (!message.guild.member(client.user).hasPermission("MUTE_MEMBERS"))
       return message
-        .reply("**I Don't Have `MUTE_MEMBERS` Permission**")
+        .reply("I Don't Have `MUTE_MEMBERS` Permission")
         .then(msg => msg.delete(6000));
 
     if (message.mentions.users.size === 0) {
-      return message.reply("Menition member");
+      return message.reply("pleade mention someone");
     }
     let muteMember = message.guild.member(message.mentions.users.first());
     if (!muteMember) {
@@ -32,7 +32,7 @@ Discord.MessagEmbed()
       .addField(`**Member vmute:**`, `<@${Member.user.id}>`)
       .addField(`**vmute by:**`, `<@${message.author.id}>`)
       .setFooter(`${message.author.username}`)
-      .setTimestamp();
+      .setTimestamp()
 );
     }
   }
