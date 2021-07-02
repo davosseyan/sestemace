3c8c67c3dffbdea93ba2f608d34a02653de6a109
hede seyan <78200081+davosseyan@users.noreply.github.com>
2021-07-03 02:59:10 +0300

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -11,36 +11,36 @@ module.exports = {
     
   message.channel.send(`\`\`\`[vote]()
 **General Commands**
-A!userinfo - Get your/someones information on Discord
-A!server - Get the server information
-A!botinfo - Zalm bot info
-A!weather - To know world's weather | usage : A!weather [state/region]
-A!avatar - To get you/someones avatar
-A!serveravatar - To get server's avatar
-A!invite - To invite Ace bot to your server
-A!roles - To show role server
-A!listEmoji - Shows server's emojis list A!numberinv - For View number your invite
+r!userinfo - Get your/someones information on Discord
+r!server - Get the server information
+r!botinfo - Zalm bot info
+r!weather - To know world's weather | usage : A!weather [state/region]
+r!avatar - To get you/someones avatar
+r!serveravatar - To get server's avatar
+r!invite - To invite Ace bot to your server
+r!roles - To show role server
+r!listEmoji - Shows server's emojis list A!numberinv - For View number your invite
 **Moderation Commands**
-A!backup - To copy server
-A!mute - Mute someone
-A!unmute - Unmute someone
-A!ban - Ban someone in the server
-A!unban - Unban someone in the server
-A!kick - To kick someone in the server
-A!clear - Clear chat messages
-A!lock - To lock the channel from
-A!unlock - To unlock the channel from
-A!hide - A!hide <#channel> , hide channel for everyone
-A!show - A!show <#channel> , show the hiden channel
-A!vkick - To kick someone in voice
-A!vmute - To mute someones mic
-A!vunmute - Unmute the person in voice
-A!deafen - To deaf someone in the voice
-A!undeafen - Remove the deafen
-A!moveall - to move all members
-A!slowmode - Set the channel slowmode
-A!allbots - For View All bot in server
-A!roleinfo - for view role info
+r!backup - To copy server
+r!mute - Mute someone
+r!unmute - Unmute someone
+r!ban - Ban someone in the server
+r!unban - Unban someone in the server
+r!kick - To kick someone in the server
+r!clear - Clear chat messages
+r!lock - To lock the channel from
+r!unlock - To unlock the channel from
+r!hide - A!hide <#channel> , hide channel for everyone
+r!show - A!show <#channel> , show the hiden channel
+r!vkick - To kick someone in voice
+r!vmute - To mute someones mic
+r!vunmute - Unmute the person in voice
+r!deafen - To deaf someone in the voice
+r!undeafen - Remove the deafen
+r!moveall - to move all members
+r!slowmode - Set the channel slowmode
+r!allbots - For View All bot in server
+r!roleinfo - for view role info
 \`\`\``);
   
     }
