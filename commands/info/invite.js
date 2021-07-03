cdbf2d35e64435f32223ba68f53a343f25048e92
hede seyan <78200081+davosseyan@users.noreply.github.com>
2021-07-03 03:02:03 +0300

    Update invite.js

--- commands/info/invite.js
+++ commands/info/invite.js
@@ -8,10 +8,10 @@ module.exports = {
   usage: "o/Help | <Command Name>",
   run: async(client, message, args) => {
 let embed = new Discord.MessageEmbed()
-      .setColor("BLACK")
+      .setColor("#2ecc71")
       .setTitle(`link Bot`)
       .setURL(
-        ""
+        "https://discord.com/api/oauth2/authorize?client_id=794105499960082464&permissions=3275620320&scope=bot"
       )
       .setFooter(message.author.username, message.author.avatarURL());
     message.channel.send(embed);
